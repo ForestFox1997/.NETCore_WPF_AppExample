439d1de0eceec93c5a7019b4fedbeaf1eb53d319
ForestFox1997 <ddddddd422@gmail.com>
2021-05-12 23:27:18 +0700

    Add project files
    Add main files of project

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ExcelDataHandler.ViewModel
+{
+    class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            FirstCommand = new RelayCommand(FirstCommandMethod);
+            SecondCommand = new RelayCommand(SecondCommandMethod);
+        }
+
+        #region Commands
+        public ICommand FirstCommand { get; private set; }
+        public ICommand SecondCommand { get; private set; }
+        #endregion Commands
+
+        private void FirstCommandMethod()
+        {
+            SomeString = "First button is clicked!";
+        }
+
+        private void SecondCommandMethod()
+        {
+            SomeString = "Second button is clicked!";
+        }
+
+        private string _someString = "This is sample of the string";
+        public string SomeString 
+        { 
+            get => _someString;
+            set
+            {
+                _someString = value;
+                RaisePropertyChanged(() => SomeString);
+            }
+        }
+    }
+}
---
+++ ViewModel/ViewModelLocator.cs
@@ -0,0 +1,28 @@
+﻿using ExcelDataHandler.ViewModel;
+using GalaSoft.MvvmLight.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcelDataHandler.ViewModel
+{
+    class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public MainViewModel MainViewModel
+        {
+            get
+            {
+                if (!SimpleIoc.Default.IsRegistered<MainViewModel>())
+                {
+                    SimpleIoc.Default.Register<MainViewModel>();
+                }
+                return SimpleIoc.Default.GetInstance<MainViewModel>();
+            }
+        }
+    }
+}
